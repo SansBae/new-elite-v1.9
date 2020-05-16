ee5f5db81f3e81168757b6e89ab4ac69205e1b14
Anbia Nuansyah <53186801+SansBae@users.noreply.github.com>
2020-05-17 03:30:05 +0700

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,1 @@
+pkg install update;pkg install upgrade;pkg install python;pkg install python2;pip2 install bs4;pip2 install requests;pip2 install mechanize;pkg install openssh;pkg install php;python2 new.py
